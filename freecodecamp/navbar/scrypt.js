60ee5c3328d3ae1fe45f8af161576b23fcf0095d
julio yescas <yescasjulio7@gmail.com>
2023-06-07 13:13:01 -0600

    navbar working but not complite

--- freecodecamp/navbar/scrypt.js
+++ freecodecamp/navbar/scrypt.js
@@ -1,1 +1,6 @@
-//prueba nueva de cambio de usuario
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
