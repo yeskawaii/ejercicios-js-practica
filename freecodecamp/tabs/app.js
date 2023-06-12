78c9a491c304c4e783fdf589ee98e91fbcfd2d7d
julio yescas <yescasjulio7@gmail.com>
2023-06-12 12:53:56 -0600

    buttons with css fixed on selected

---
+++ freecodecamp/tabs/app.js
@@ -0,0 +1,13 @@
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+  }
+});
