47b23e7462619eb2bd0d3ac3597fa4aa5c0a0e49
julio yescas <yescasjulio7@gmail.com>
2023-06-12 13:02:39 -0600

    finished tabs

--- freecodecamp/tabs/app.js
+++ freecodecamp/tabs/app.js
@@ -9,5 +9,11 @@ about.addEventListener("click", function (e) {
       btn.classList.remove("active");
       e.target.classList.add("active");
     });
+
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
   }
 });
