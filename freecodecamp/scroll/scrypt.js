83b4c47c1e4693fd4bcdd37781298debd9f2dc65
julio yescas <yescasjulio7@gmail.com>
2023-06-12 10:35:41 -0600

    dinamic nav bar, on diferrent width

--- freecodecamp/scroll/scrypt.js
+++ freecodecamp/scroll/scrypt.js
@@ -1,8 +1,22 @@
-//set date
+//set
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 //close links
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+  //linksContainer.classList.toggle("show-links");
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 //fixed navbar
 
 //smooth scroll
