efbb7e32a02b7ec18d1debe89eb61864e2628c83
julio yescas <yescasjulio7@gmail.com>
2023-06-13 11:07:44 -0600

    countdown finished

--- freecodecamp/count down/scrypt.js
+++ freecodecamp/count down/scrypt.js
@@ -26,8 +26,13 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2028, 4, 24, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
+//let futureDate = new Date(2023, 5, 13, 11, 3, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -46,7 +51,6 @@ const futureTime = futureDate.getTime();
 function getRemainingTime() {
   const today = new Date().getTime();
   const t = futureTime - today;
-  console.log(t);
   //1s = 1000ms
   //1M = 60S
   //1HR = 60MIN
@@ -66,9 +70,21 @@ function getRemainingTime() {
   // set vallues array;
   const values = [days, hours, minutes, seconds];
 
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
   items.forEach(function (item, index) {
-    item.innerHTML = values[index];
+    item.innerHTML = format(values[index]);
   });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+  }
 }
 
+let countdown = setInterval(getRemainingTime, 1000);
 getRemainingTime();
