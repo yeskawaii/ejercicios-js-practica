74d4deb512fbc18cdef44b987aeba5d9bd4ef61d
julio yescas <yescasjulio7@gmail.com>
2023-06-13 09:50:58 -0600

    checkpoint on date

--- freecodecamp/count down/scrypt.js
+++ freecodecamp/count down/scrypt.js
@@ -21,3 +21,18 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelector(".deadline-format h4");
+
+let futureDate = new Date(2028, 4, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+console.log(months[month - 1]);
+
+giveaway.textContent = `giveaway ends on ${year} ${hours}:${minutes}am`;
