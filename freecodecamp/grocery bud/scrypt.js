3cd1b079ed93c573aa25a3e9dcdaa4ed0e87b1d2
julio yescas <yescasjulio7@gmail.com>
2023-06-16 12:05:07 -0600

    adding the elements on the list (input)

--- freecodecamp/grocery bud/scrypt.js
+++ freecodecamp/grocery bud/scrypt.js
@@ -27,6 +27,27 @@ function addItem(e) {
     const element = document.createElement("article");
     //add class
     element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    //append child
+    list.appendChild(element);
+    //display alert
+    displayAlert("item added to the list", "success");
+    //show container
+    container.classList.add("show-container");
   } else if (value && editFlag) {
     console.log("editing");
   } else {
